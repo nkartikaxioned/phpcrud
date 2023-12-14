a48d5a3038d67190b740bf7c6b8b365b75c99e2f
nkartikaxioned <kartikn@axioned.com>
2023-12-14 17:46:23 +0530

    css file add and registration form rename to index.php

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ ini_set('display_errors', 1);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>PHP CRUD</title>
+  <link rel="stylesheet" media="screen" href="./assets/css/style.css">
 </head>
 
 <body>
